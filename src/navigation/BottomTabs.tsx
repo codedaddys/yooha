003edb8d912e79284bc8026a26c631f36354a7da
John Haupenthal <johnhaup@gmail.com>
2025-01-18 22:01:33 -0800

    feat: add tab icons

--- src/navigation/BottomTabs.tsx
+++ src/navigation/BottomTabs.tsx
@@ -1,19 +1,29 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { SearchStack } from "./SearchStack";
 import { AccountStack } from "./AccountStack";
+import SearchSvg from "../../assets/svgs/search.svg";
+import UserSvg from "../../assets/svgs/user.svg";
 
 export const BottomTabs = createBottomTabNavigator({
+  screenOptions: {
+    tabBarShowLabel: false,
+    tabBarStyle: {
+      paddingTop: 4,
+    },
+  },
   screens: {
     SearchTab: {
       screen: SearchStack,
       options: {
         headerShown: false,
+        tabBarIcon: SearchSvg,
       },
     },
     AccountTab: {
       screen: AccountStack,
       options: {
         headerShown: false,
+        tabBarIcon: UserSvg,
       },
     },
   },
